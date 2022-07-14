4c17051851d5196e5ee9ef27e780a026bae48d8e
sky <shn7446@duksung.ac.kr>
2022-07-14 21:11:46 +0900

    feat: ActivityPage 정보 받아오기 완료

--- cornerlog_client/src/components/StudyCard.js
+++ cornerlog_client/src/components/StudyCard.js
@@ -1,7 +1,8 @@
-import React from 'react';
-import styled from '../css/ActivityPage.module.css';
+import React from "react";
+import styled from "../css/ActivityPage.module.css";
 
 const StudyCard = ({ data }) => {
+  const content = data.content?.substr(0, 280);
   return (
     <div className={styled.CardBlock}>
       <p className={styled.HeadingText}>
@@ -9,14 +10,8 @@ const StudyCard = ({ data }) => {
           {data.title}
         </a>
       </p>
-      <p className={styled.Content}>{data.content}
-        리덕스 라이브러리 사용 시 이점
-        상태 업데이트에 관한 로직을 따로 분리해 모듈화
-        여러 컴포넌트에서 동일한 상태 공유
-        업데이트가 필요한 컴포넌트만 리렌더링되도록 최적화
-        여러 컴포넌트에서 동일한 상태 공유
-        업데이트가 필요한 컴포넌트만 리렌더링되도록 최적화</p>
+      <p className={styled.Content}>{content}</p>
     </div>
-  )
-}
+  );
+};
 export default StudyCard;
--- cornerlog_client/src/pages/ActivityPage.js
+++ cornerlog_client/src/pages/ActivityPage.js
@@ -1,107 +1,146 @@
-import React, { useEffect, useState } from 'react';
-import Axios from 'axios';
-import StudyCard from '../components/StudyCard';
-import StudyCardTitle from '../components/StudyCardTitle';
-import styles from '../css/ActivityPage.module.css';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faArrowRight } from '@fortawesome/free-solid-svg-icons';
+import React, { useEffect, useState } from "react";
+import Axios from "axios";
+import StudyCard from "../components/StudyCard";
+import StudyCardTitle from "../components/StudyCardTitle";
+import styles from "../css/ActivityPage.module.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faArrowRight } from "@fortawesome/free-solid-svg-icons";
 const ActivityPage = () => {
-  const [category, setCategory] = useState("Corner");
+  const [category, setCategory] = useState("");
   const [representImg, setRepresentImg] = useState();
-  const [cDataArray, setcDataArray] = useState([]);
   const [data, setData] = useState([]);
   const [count, setCount] = useState(15);
   const [dataArray, setDataArray] = useState([]);
+  const [result, setResult] = useState([]);
+  const [spinner, setSpinner] = useState(true);
   let tempArray = [];
+
   useEffect(() => {
-    console.log(process.env.REACT_APP_TISTORY_ACCESS_TOKEN)
-    Axios
-      .get(`https://www.tistory.com/apis/post/list?access_token=${process.env.REACT_APP_TISTORY_ACCESS_TOKEN}&output=json&blogName=corner-ds&page=1`)
+    console.log(process.env.REACT_APP_TISTORY_ACCESS_TOKEN);
+    Axios.get(
+      `https://www.tistory.com/apis/post/list?access_token=${process.env.REACT_APP_TISTORY_ACCESS_TOKEN}&output=json&blogName=corner-ds&page=1`
+    )
       // eslint-disable-next-line no-loop-func
       .then((response) => {
         setCount(response.data.tistory.item.totalCount);
-      })
-  }, [])
+      });
+  }, []);
 
   useEffect(() => {
     for (let pageNumber = 1; pageNumber < count / 10 + 1; pageNumber++) {
-      Axios
-        .get(`https://www.tistory.com/apis/post/list?access_token=${process.env.REACT_APP_TISTORY_ACCESS_TOKEN}&output=json&blogName=corner-ds&page=${pageNumber}`)
-        .then((response) => {
-          // console.log(response.data);
-          if (response.data.tistory.item.posts) {
-            tempArray.push(...response.data.tistory.item.posts);
-          }
-          if (tempArray.length >= count) {
-            // id순으로 정렬
-            setData(tempArray.sort(function (a, b) {
+      Axios.get(
+        `https://www.tistory.com/apis/post/list?access_token=${process.env.REACT_APP_TISTORY_ACCESS_TOKEN}&output=json&blogName=corner-ds&page=${pageNumber}`
+      ).then((response) => {
+        // console.log(response.data);
+        if (response.data.tistory.item.posts) {
+          tempArray.push(...response.data.tistory.item.posts);
+        }
+        if (tempArray.length >= count) {
+          // id순으로 정렬
+          setData(
+            tempArray.sort(function (a, b) {
               return a.id - b.id;
-            }));
-          }
-        })
+            })
+          );
+        }
+      });
     }
-  }, [count])
+  }, [count]);
 
   useEffect(() => {
-    console.log(data)
     // categoryId별로 나누기
-    const categoryList = {
-      "ReactStarter1": data.filter(data => data.categoryId === "1015295"),
-      "ReactStarter2": data.filter(data => data.categoryId === "1015294"),
-      "ReactMaster": data.filter(data => data.categoryId === "1015296"),
-      "NodeJS": data.filter(data => data.categoryId === "1015297"),
-      "Corner": data.filter(data => data.categoryId === "1015293")
-    };
-    setDataArray(categoryList)
-  }, [data])
-
-  useEffect((category) => {
-    if (category === "Corner") {
-      setDataArray(dataArray.Corner)
+    if (data && Object.keys(data).length >= count) {
+      const categoryList = {
+        ReactStarter1: data.filter((data) => data.categoryId === "1015294"),
+        ReactStarter2: data.filter((data) => data.categoryId === "1015295"),
+        ReactMaster: data.filter((data) => data.categoryId === "1015296"),
+        NodeJS: data.filter((data) => data.categoryId === "1015297"),
+        Corner: data.filter((data) => data.categoryId === "1015293"),
+      };
+      setDataArray(categoryList);
     }
-    console.log("dma", dataArray)
-  }, [category])
-
+  }, [data]);
 
   useEffect(() => {
-    // 카테고리 별 대표이미지 변경
-    console.log(category);
+    // 카테고리 별로 최신 4개 글 postid
+    if (dataArray && Object.entries(dataArray).length > 0) {
+      for (const [key, value] of Object.entries(dataArray)) {
+        Axios.all([
+          Axios.get(
+            `https://www.tistory.com/apis/post/read?access_token=${
+              process.env.REACT_APP_TISTORY_ACCESS_TOKEN
+            }&output=json&blogName=corner-ds&postId=${
+              value[value.length - 1].id
+            }`
+          ),
+          Axios.get(
+            `https://www.tistory.com/apis/post/read?access_token=${
+              process.env.REACT_APP_TISTORY_ACCESS_TOKEN
+            }&output=json&blogName=corner-ds&postId=${
+              value[value.length - 2].id
+            }`
+          ),
+          Axios.get(
+            `https://www.tistory.com/apis/post/read?access_token=${
+              process.env.REACT_APP_TISTORY_ACCESS_TOKEN
+            }&output=json&blogName=corner-ds&postId=${
+              value[value.length - 3].id
+            }`
+          ),
+          Axios.get(
+            `https://www.tistory.com/apis/post/read?access_token=${
+              process.env.REACT_APP_TISTORY_ACCESS_TOKEN
+            }&output=json&blogName=corner-ds&postId=${
+              value[value.length - 4].id
+            }`
+          ),
+        ]).then(
+          Axios.spread((...responses) => {
+            // console.log(responses);
+            responses.forEach((data, index) => {
+              let array = data.data.tistory.item.content.replace(
+                /<[^>]*>?/g,
+                ""
+              );
+              value[value.length - index - 1]["content"] = array;
+              index--;
+            });
+            setCategory("Corner");
+            setResult(dataArray.Corner);
+            setSpinner(false);
+          })
+        );
+      }
+    }
+  }, [dataArray]);
+
+  useEffect(() => {
+    // 카테고리 별 대표이미지, 글 목록(내용 포함)
     if (category === "Corner") {
       setRepresentImg("img/corner_logo.png");
-    }
-    else if (category === "ReactStarter1") {
+      setResult(dataArray.Corner);
+    } else if (category === "ReactStarter1") {
       setRepresentImg("img/rs1.jpg");
-    }
-    else if (category === "ReactStarter2") {
+      setResult(dataArray.ReactStarter1);
+    } else if (category === "ReactStarter2") {
       setRepresentImg("img/rs2.jpg");
-    }
-    else if (category === "ReactMaster") {
+      setResult(dataArray.ReactStarter2);
+    } else if (category === "ReactMaster") {
       setRepresentImg("img/rm.jpg");
-    }
-    else if (category === "NodeJS") {
+      setResult(dataArray.ReactMaster);
+    } else if (category === "NodeJS") {
       setRepresentImg("img/nj.png");
+      setResult(dataArray.NodeJS);
     }
-  }, [category])
+  }, [category, dataArray]);
 
-  function CategoryData() {
-    // 카테고리 별 데이터를 목록에 불러오기 위해
-    // 해당 카테고리 데이터 반환
-    if (category === "Corner") {
-      return dataArray.Corner;
+  useEffect(() => {
+    console.log(result);
+    if (result && result.length && result[result.length - 1].content) {
+      setSpinner(false);
     }
-    else if (category === "ReactStarter1") {
-      return dataArray.ReactStarter1;
-    }
-    else if (category === "ReactStarter2") {
-      return dataArray.ReactStarter2;
-    }
-    else if (category === "ReactMaster") {
-      return dataArray.ReactMaster;
-    }
-    else if (category === "NodeJS") {
-      return dataArray.NodeJS;
-    }
-  }
+  }, [result]);
+
   return (
     <div className={styles.body}>
       <section className={styles.left}>
@@ -109,42 +148,77 @@ const ActivityPage = () => {
           <img src={representImg} />
         </div>
         <ul className={styles.BoardTitleList}>
-          <div className={styles.BoardTitleDiv}><li className={styles.BoardTitle} onClick={() => setCategory("Corner")}>Corner</li><FontAwesomeIcon icon={faArrowRight} /></div>
-          <div className={styles.BoardTitleDiv}><li className={styles.BoardTitle} onClick={() => setCategory("ReactStarter1")}>React Starter 1</li><FontAwesomeIcon icon={faArrowRight} /></div>
-          <div className={styles.BoardTitleDiv}><li className={styles.BoardTitle} onClick={() => setCategory("ReactStarter2")}>React Starter 2</li><FontAwesomeIcon icon={faArrowRight} /></div>
-          <div className={styles.BoardTitleDiv}><li className={styles.BoardTitle} onClick={() => setCategory("ReactMaster")}>React Master</li><FontAwesomeIcon icon={faArrowRight} /></div>
-          <div className={styles.BoardTitleDiv}><li className={styles.BoardTitle} onClick={() => setCategory("NodeJS")}>Node.js</li><FontAwesomeIcon icon={faArrowRight} /></div>
+          <div className={styles.BoardTitleDiv}>
+            <li
+              className={styles.BoardTitle}
+              onClick={() => setCategory("Corner")}
+            >
+              Corner
+            </li>
+            <FontAwesomeIcon icon={faArrowRight} />
+          </div>
+          <div className={styles.BoardTitleDiv}>
+            <li
+              className={styles.BoardTitle}
+              onClick={() => setCategory("ReactStarter1")}
+            >
+              React Starter 1
+            </li>
+            <FontAwesomeIcon icon={faArrowRight} />
+          </div>
+          <div className={styles.BoardTitleDiv}>
+            <li
+              className={styles.BoardTitle}
+              onClick={() => setCategory("ReactStarter2")}
+            >
+              React Starter 2
+            </li>
+            <FontAwesomeIcon icon={faArrowRight} />
+          </div>
+          <div className={styles.BoardTitleDiv}>
+            <li
+              className={styles.BoardTitle}
+              onClick={() => setCategory("ReactMaster")}
+            >
+              React Master
+            </li>
+            <FontAwesomeIcon icon={faArrowRight} />
+          </div>
+          <div className={styles.BoardTitleDiv}>
+            <li
+              className={styles.BoardTitle}
+              onClick={() => setCategory("NodeJS")}
+            >
+              Node.js
+            </li>
+            <FontAwesomeIcon icon={faArrowRight} />
+          </div>
         </ul>
       </section>
       <section className={styles.right}>
-        {(CategoryData()?.length > 0) ?
+        {result && result.length > 0 && (
           <>
-            <StudyCard data={CategoryData()[CategoryData().length - 1]}></StudyCard>
-            <StudyCard data={CategoryData()[CategoryData().length - 2]}></StudyCard>
-            <StudyCard data={CategoryData()[CategoryData().length - 3]}></StudyCard>
-            <StudyCard data={CategoryData()[CategoryData().length - 4]}></StudyCard>
+            <StudyCard data={result[result.length - 1]}></StudyCard>
+            <StudyCard data={result[result.length - 2]}></StudyCard>
+            <StudyCard data={result[result.length - 3]}></StudyCard>
+            <StudyCard data={result[result.length - 4]}></StudyCard>
           </>
-          :
-          <div>dataArray.{category}스피너 업로드 예정</div>
-        }
-
-        {(CategoryData()?.length > 0) ?
+        )}
+        {result && result.length > 0 ? (
           <div className={styles.cardTitleList}>
             <hr />
-            <StudyCardTitle data={CategoryData()[CategoryData().length - 1]} />
-            <StudyCardTitle data={CategoryData()[CategoryData().length - 2]} />
-            <StudyCardTitle data={CategoryData()[CategoryData().length - 3]} />
-            <StudyCardTitle data={CategoryData()[CategoryData().length - 4]} />
+            <StudyCardTitle data={result[result?.length - 5]} />
+            <StudyCardTitle data={result[result?.length - 6]} />
+            <StudyCardTitle data={result[result?.length - 7]} />
+            <StudyCardTitle data={result[result?.length - 8]} />
           </div>
-          :
-          <div>글 목록 업로드 예정</div>
-        }
-        <div className={styles.pagination}>
-          페이지네이션 추가 예정
-        </div>
+        ) : (
+          <div></div>
+        )}
+        <div className={styles.pagination}>=</div>
       </section>
     </div>
-  )
-}
+  );
+};
 
 export default ActivityPage;
