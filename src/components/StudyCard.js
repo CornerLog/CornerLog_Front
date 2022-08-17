866ea2a15fbd6f11dce3954bde20a64fec0dee5a
soosoo030 <whereru511@gmail.com>
2022-08-17 23:01:28 +0900

    Activity 모바일 페이지 수정

--- src/components/StudyCard.js
+++ src/components/StudyCard.js
@@ -4,13 +4,16 @@ import styled from "../css/ActivityPage.module.css";
 const StudyCard = ({ data }) => {
   const content = data.content?.substr(0, 280);
   return (
+    
     <div className={styled.CardBlock}>
+      <a href={data.postUrl} target="_blank" className={styled.TitleLink}>
       <p className={styled.HeadingText}>
         <a href={data.postUrl} target="_blank" className={styled.TitleLink}>
           {data.title}
         </a>
       </p>
       <p className={styled.Content}>{content}</p>
+      </a>
     </div>
   );
 };
